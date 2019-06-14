e15378d46af1b454562cd13c0eca8868d2c2a82e
Darwin Lyons <whoischarles@gmail.com>
2019-06-14 14:59:43 -0400

    trying to fix live site

--- src/header.js
+++ src/header.js
@@ -4,7 +4,6 @@ const Header = () => {
   return (
     <div>
       <h1>Send a wish into the Interverse</h1>
-      <p>Type a wish below and it will be added to the cloud of wishes written by other visitors to this site.</p>
     </div>
   )
 } 
